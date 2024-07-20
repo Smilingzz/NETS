970d0bef390b9b320fdce9a4e82c6906a0ee8d00
Smilingzz <alexander.7938@gmail.com>
2024-07-20 21:17:03 +0200

    Configured the code better, added more in README

--- NETS.py
+++ NETS.py
@@ -660,26 +660,3 @@ class NETS:
             if ss >= threshold:
                 return False
         return True
-
-
-amount = {}
-amount[1] = 3
-amount[7] = 5
-amount[8] = 5
-id = 0
-windowSize = 3
-slideSize = 1
-Window = {}
-nets = NETS(dim=1, subDim=1, R=2.0, k=6, S=slideSize, W=windowSize, nW=1, maxValues=[8], minValues=[1])
-for j in range(windowSize):
-    for val in amount:
-        for i in range(amount[val]):
-            if j not in Window:
-                Window[j] = []
-            data = Tuple(id, j, [val])
-            id += 1
-            Window[j].append(data)
-    nets.calcNetChange(Window[j], j)
-    nets.findOutlier(j)
-    outliers = nets.returnOutliers()
-    print(outliers)
--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 NETS Outlier Detection: Python Implementation
 
 All credit goes to the paper published in PVLDB 2019 [[Paper](http://www.vldb.org/pvldb/vol12/p1303-yoon.pdf)] and the GitHub repository https://github.com/kaist-dmlab/NETS 
+
+### Getting started
+- NETS supports multi-dimensioned data. Given a set value ```R``` and ```k``` it determines an exact solution of outliers. 
+- To use NETS, import the classes into any python project of choice. Make sure to read the parameters of NETS.
+- A minor test-case can be configured to test NETS, simply run ```testNETS.py```.
